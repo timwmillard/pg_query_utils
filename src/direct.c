e09fd91615ac90bd30ff5beae1e9c8227d4d6855
Tim Millard <timwmillard@gmail.com>
2024-10-29 23:01:45 +1100

    walk_node

--- src/direct.c
+++ src/direct.c
@@ -58,26 +58,37 @@ typedef struct {
     int level;
 } NodeContext;
 
-bool find_node(Node *node, NodeContext *ctx) {
+bool walk_node(Node *node, NodeContext *ctx) 
+{
     if (node == NULL) return false;
 
-    for (int i; i<ctx->level; i++) printf(" ");
+    // Print some space for each level
+    for (int i=0; i < ctx->level; i++) printf("   ");
 
-    printf("NODE(%d) ", node->type);
-    
-    /*if (node->type == 62) return true;*/
+    // Process Node
+    printf("NODE(%d): %d ", node->type, ctx->level);
     switch (node->type) {
-        /*case T_InsertStmt: printf(" INSERT"); break;*/
-        /*case T_UpdateStmt: printf(" UPDATE"); break;*/
-        /*case T_SelectStmt: printf(" SELECT"); break;*/
 #include "nodetags.h"
         default: break;
     }
     printf("\n");
+    // End Process Node
+
+    if (IsA(node, List)) {
+        ctx->level++;
+        List *list = (List*) node;
+        ListCell *cell;
+        foreach(cell, list) {
+            Node *node = lfirst(cell);
+            walk_node(node, ctx);
+        }
+        ctx->level--;
+        return false;
+    }
 
     bool result;
     ctx->level++;
-    result = raw_expression_tree_walker(node, find_node, ctx);
+    result = raw_expression_tree_walker(node, walk_node, ctx);
     ctx->level--;
     return result;
 }
@@ -110,7 +121,7 @@ int main() {
         printf("STATEMENT\n");
         bool result;
         /*result = raw_expression_tree_walker(node, find_node, ctx);*/
-        result = find_node(node, &node_ctx);
+        result = walk_node(node, &node_ctx);
     }
 
     return 0;
